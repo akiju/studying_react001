cc7177998807d2d566f6738d0f1705db47ec29aa
akiju <99388105+akiju@users.noreply.github.com>
2022-02-15 18:51:50 +0900

    サンプル　Step001まで TODOリストの追加まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputtext = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div 生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //li 生成
+  const li = document.createElement("li");
+  li.innerText = inputtext;
+  //divの子要素にli等を入れる
+  div.appendChild(li);
+
+  //未完成リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document.getElementById("add-button").addEventListener("click", function () {
+  return onClickAdd();
+});
